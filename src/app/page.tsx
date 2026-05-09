899e5a212a67c0ed65d10ffa4688723e31a1187e
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-05-09 15:47:15 +0600

    refactor: remove Suspense wrapper from HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import { ModelsPage } from '@/features/models'
-import { Suspense } from 'react'
 
 export default function HomePage() {
-  return (
-    <Suspense fallback={null}>
-      <ModelsPage />
-    </Suspense>
-  )
+  return <ModelsPage />
 }
